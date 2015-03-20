42e7a253fca803c2075e56001a49e1953780490f
barragan <barragan@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-20 15:01:18 +0000

    Tagging version 1.0
    git-svn-id: https://plugins.svn.wordpress.org/wp-revisions-limit/tags/1.0@1116928 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/class-wp-revisions-limit-activator.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @since      1.0.0
+ *
+ * @package    Wp_Revisions_Limit
+ * @subpackage Wp_Revisions_Limit/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Wp_Revisions_Limit
+ * @subpackage Wp_Revisions_Limit/includes
+ * @author     Roger Rodrigo
+ */
+class Wp_Revisions_Limit_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
